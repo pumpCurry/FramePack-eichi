8406c4ba020069e3286d98e7af259425c03442f4
pumpCurry <pcb@hogera.com>
2025-07-26 23:27:13 +0900

    Improve spinner stdout compatibility

--- webui/eichi_utils/spinner.py
+++ webui/eichi_utils/spinner.py
@@ -15,14 +15,27 @@ def spinner_while_running(message, function, *args, **kwargs):
     original_stdout = sys.stdout
 
     class LockedStdout:
+        def __init__(self, original):
+            self._original = original
+
         def write(self, s):
             with lock:
-                original_stdout.write(s)
-                original_stdout.flush()
+                self._original.write(s)
+                self._original.flush()
 
         def flush(self):
             with lock:
-                original_stdout.flush()
+                self._original.flush()
+
+        def fileno(self):
+            return self._original.fileno()
+
+        def isatty(self):
+            return self._original.isatty()
+
+        @property
+        def encoding(self):
+            return getattr(self._original, "encoding", "utf-8")
 
     def spinner():
         with lock:
@@ -40,7 +53,7 @@ def spinner_while_running(message, function, *args, **kwargs):
             original_stdout.flush()
 
     spinner_thread = threading.Thread(target=spinner)
-    sys.stdout = LockedStdout()
+    sys.stdout = LockedStdout(original_stdout)
     spinner_thread.start()
     try:
         result = function(*args, **kwargs)
